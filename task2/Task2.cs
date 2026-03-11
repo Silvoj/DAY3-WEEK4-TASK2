f73826cbb564f546a3b54b96883acc699b7daef1
Silvoj <akshayasilvoj@gmail.com>
2026-03-11 17:38:26 +0530

    Add project files.

---
+++ task2/Task2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task2
+{
+    internal class Task2
+    {
+        static void Main() {
+            Console.Write("Enter First Number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Second Number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator (+, -, *, /): ");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            double result;
+            switch (op)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        Console.WriteLine("Result: " + result);
+                        break;
+
+                    case '-':
+                        result = num1 - num2;
+                        Console.WriteLine("Result: " + result);
+                        break;
+
+                    case '*':
+                        result = num1 * num2;
+                        Console.WriteLine("Result: " + result);
+                        break;
+
+                    case '/':
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("Division by zero not allowed");
+                        }
+                        else
+                        {
+                            result = num1 / num2;
+                            Console.WriteLine("Result: " + result);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Operator");
+                        break;
+                }
+            }
+        }
+
+
+
+    }
+   
